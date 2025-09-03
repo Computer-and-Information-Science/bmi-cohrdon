667bff1a7bef9bfa9912b0815cefaaf16677d3e0
cohrdon <kaden.cordon@student.northampton.edu>
2025-09-03 15:25:11 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main() {
- 
+ int height = 61; 
+ int weight =130;
 
+double  kg = weight * .453592;
+double m = height * 0.0254;
+double BMI = kg / (m * m); 
 
+cout << "the BMI for this person is:  = " << BMI << endl;
 }
